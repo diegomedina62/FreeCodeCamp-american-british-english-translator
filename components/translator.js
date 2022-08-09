da55b41e126b244125866c463e191c520da1d0e3
diegomedina62 <diegomedina62@gmail.com>
2022-08-09 12:36:50 -0300

    code for translation api

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,107 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
+const spellingsAtoB = new Map(Object.entries(americanToBritishSpelling));
+const titlesAtoB = new Map(Object.entries(americanToBritishTitles));
+
+const spellingBtoA = new Map();
+Object.entries(americanToBritishSpelling).forEach((x) => {
+  spellingBtoA.set(x[1], x[0]);
+});
+const titlesBtoA = new Map();
+Object.entries(americanToBritishTitles).forEach((x) => {
+  titlesBtoA.set(x[1], x[0]);
+});
 
 class Translator {
+  AmToBri(text) {
+    let translation = text;
 
+    Object.keys(americanOnly).forEach((x) => {
+      if (translation.indexOf(x) >= 0) {
+        translation = translation.replace(
+          x,
+          `<span class="highlight">${americanOnly[x]}</span>`
+        );
+      }
+    });
+    spellingsAtoB.forEach((value, key) => {
+      if (translation.indexOf(key) >= 0) {
+        translation = translation.replace(
+          key,
+          `<span class="highlight">${value}</span>`
+        );
+      }
+    });
+    titlesAtoB.forEach((value, key) => {
+      if (translation.indexOf(key) >= 0) {
+        translation = translation.replace(
+          key,
+          `<span class="highlight">${value}</span>`
+        );
+      }
+    });
+    if (/([1-9]|1[012]):[0-5][0-9]/g.test(translation)) {
+      const hour = translation.match(/([1-9]|1[012]):[0-5][0-9]/g);
+      let hourArr = [];
+      hour.forEach((x) => {
+        hourArr.push([x, x.replace(":", ".")]);
+      });
+
+      hourArr.forEach((x) => {
+        translation = translation.replace(
+          x[0],
+          `<span class="highlight">${x[1]}</span>`
+        );
+      });
+    }
+
+    return translation;
+  }
+  BriToAm(text) {
+    let translation = text;
+
+    Object.keys(britishOnly).forEach((x) => {
+      if (translation.indexOf(x) >= 0) {
+        translation = translation.replace(
+          x,
+          `<span class="highlight">${britishOnly[x]}</span>`
+        );
+      }
+    });
+    spellingBtoA.forEach((value, key) => {
+      if (translation.indexOf(key) >= 0) {
+        translation = translation.replace(
+          key,
+          `<span class="highlight">${value}</span>`
+        );
+      }
+    });
+    titlesBtoA.forEach((value, key) => {
+      if (translation.indexOf(key) >= 0) {
+        translation = translation.replace(
+          key,
+          `<span class="highlight">${value}</span>`
+        );
+      }
+    });
+    if (/([1-9]|1[012])\.[0-5][0-9]/g.test(translation)) {
+      const hour = translation.match(/([1-9]|1[012])\.[0-5][0-9]/g);
+      let hourArr = [];
+      hour.forEach((x) => {
+        hourArr.push([x, x.replace(".", ":")]);
+      });
+
+      hourArr.forEach((x) => {
+        translation = translation.replace(
+          x[0],
+          `<span class="highlight">${x[1]}</span>`
+        );
+      });
+    }
+    return translation;
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,37 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+
+    if (text == undefined || !locale) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+    if (text == "") {
+      return res.json({ error: "No text to translate" });
+    }
+    let translation = "";
+    if (locale == "american-to-british") {
+      translation = translator.AmToBri(text);
+    } else if (locale == "british-to-american") {
+      translation = translator.BriToAm(text);
+    } else {
+      return res.json({ error: "Invalid value for locale field" });
+    }
+
+    if (translation == text) {
+      return res.json({
+        text: text,
+        translation: "Everything looks good to me!",
+      });
+    }
+    res.json({
+      text: text,
+      translation: translation,
     });
+  });
 };
