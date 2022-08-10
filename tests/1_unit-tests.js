b756aefd82943bdefd9afcd96f37c5cc8c7903b5
diegomedina62 <diegomedina62@gmail.com>
2022-08-09 23:57:15 -0300

    coding of quality test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,184 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
+let translator = new Translator();
+suite("Unit Tests", () => {
+  suite("To British English", () => {
+    test("Mangoes are my favorite fruit.", (done) => {
+      assert.equal(
+        translator.AmToBri("Mangoes are my favorite fruit.")[1],
+        "Mangoes are my favourite fruit."
+      );
+      done();
+    });
+    test("I ate yogurt for breakfast.", (done) => {
+      assert.equal(
+        translator.AmToBri("I ate yogurt for breakfast.")[1],
+        "I ate yoghurt for breakfast."
+      );
+      done();
+    });
+    test("We had a party at my friend's condo.", (done) => {
+      assert.equal(
+        translator.AmToBri("We had a party at my friend's condo.")[1],
+        "We had a party at my friend's flat."
+      );
+      done();
+    });
+    test("Can you toss this in the trashcan for me?", (done) => {
+      assert.equal(
+        translator.AmToBri("Can you toss this in the trashcan for me?")[1],
+        "Can you toss this in the rubbishcan for me?"
+      );
+      done();
+    });
+    test("The parking lot was full.", (done) => {
+      assert.equal(
+        translator.AmToBri("The parking lot was full.")[1],
+        "The car park was full."
+      );
+      done();
+    });
+    test("Like a high tech Rube Goldberg machine.", (done) => {
+      assert.equal(
+        translator.AmToBri("Like a high tech Rube Goldberg machine.")[1],
+        "Like a high tech Rube Goldberg machine."
+      );
+      done();
+    });
+    test("To play hooky means to skip class or work.", (done) => {
+      assert.equal(
+        translator.AmToBri("To play hooky means to skip class or work.")[1],
+        "To bunk off means to skip class or work."
+      );
+      done();
+    });
+    test("No Mr. Bond, I expect you to die.", (done) => {
+      assert.equal(
+        translator.AmToBri("No Mr. Bond, I expect you to die.")[1],
+        "No Mr Bond, I expect you to die."
+      );
+      done();
+    });
+    test("Dr. Grosh will see you now.", (done) => {
+      assert.equal(
+        translator.AmToBri("Dr. Grosh will see you now.")[1],
+        "Dr Grosh will see you now."
+      );
+      done();
+    });
+    test("Lunch is at 12:15 today.", (done) => {
+      assert.equal(
+        translator.AmToBri("Lunch is at 12:15 today.")[1],
+        "Lunch is at 12.15 today."
+      );
+      done();
+    });
+  });
 
-suite('Unit Tests', () => {
-
+  suite("To American English", () => {
+    test("We watched the footie match for a while.", (done) => {
+      assert.equal(
+        translator.BriToAm("We watched the footie match for a while.")[1],
+        "We watched the soccer match for a while."
+      );
+      done();
+    });
+    test("Paracetamol takes up to an hour to work.", (done) => {
+      assert.equal(
+        translator.BriToAm("paracetamol takes up to an hour to work.")[1],
+        "Tylenol thank youkes up to an hour to work."
+      );
+      done();
+    });
+    test("First, caramelise the onions.", (done) => {
+      assert.equal(
+        translator.BriToAm("First, caramelise the onions.")[1],
+        "First, caramelize the onions."
+      );
+      done();
+    });
+    test("I spent the bank holiday at the funfair.", (done) => {
+      assert.equal(
+        translator.BriToAm("I spent the bank holiday at the funfair.")[1],
+        "I spent the barlic holiday at the carnival."
+      );
+      done();
+    });
+    test("I had a bicky then went to the chippy.", (done) => {
+      assert.equal(
+        translator.BriToAm("I had a bicky then went to the chippy.")[1],
+        "I had a cookie then went to the fish-and-fish-and-chip shop."
+      );
+      done();
+    });
+    test("I've just got bits and bobs in my bum bag.", (done) => {
+      assert.equal(
+        translator.BriToAm("I've just got bits and bobs in my bum bag.")[1],
+        "I've just got odds and ends in my fanny pack."
+      );
+      done();
+    });
+    test("The car boot sale at Boxted Airfield was called off.", (done) => {
+      assert.equal(
+        translator.BriToAm(
+          "The car boot sale at Boxted Airfield was called off."
+        )[1],
+        "The swap meet at Boxted Airfield was called off."
+      );
+      done();
+    });
+    test("Have you met Mrs Kalyani?", (done) => {
+      assert.equal(
+        translator.BriToAm("Have you met Mrs Kalyani?")[1],
+        "Have you met Mr.s Kalyani?"
+      );
+      done();
+    });
+    test("Prof Joyner of King's College, London.", (done) => {
+      assert.equal(
+        translator.BriToAm("Prof Joyner of King's College, London.")[1],
+        "Prof. Joyner of King's College, London."
+      );
+      done();
+    });
+    test("Tea time is usually around 4 or 4.30.", (done) => {
+      assert.equal(
+        translator.BriToAm("Tea time is usually around 4 or 4.30.")[1],
+        "Tea time is usually around 4 or 4:30."
+      );
+      done();
+    });
+  });
+  suite("highlight translation", () => {
+    test("Mangoes are my favorite fruit.", (done) => {
+      assert.equal(
+        translator.AmToBri("Mangoes are my favorite fruit.")[0],
+        'Mangoes are my <span class="highlight">favourite</span> fruit.'
+      );
+      done();
+    });
+    test("I ate yogurt for breakfast.", (done) => {
+      assert.equal(
+        translator.AmToBri("I ate yogurt for breakfast.")[0],
+        'I ate <span class="highlight">yoghurt</span> for breakfast.'
+      );
+      done();
+    });
+    test("We watched the footie match for a while.", (done) => {
+      assert.equal(
+        translator.BriToAm("We watched the footie match for a while.")[0],
+        'We watched the <span class="highlight">soccer</span> match for a while.'
+      );
+      done();
+    });
+    test("Paracetamol takes up to an hour to work.", (done) => {
+      assert.equal(
+        translator.BriToAm("Paracetamol takes up to an hour to work.")[0],
+        'Parace<span class="highlight">thank you</span>mol takes up to an hour to work.'
+      );
+      done();
+    });
+  });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,12 +1,98 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server.js');
+const server = require("../server.js");
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
-
-suite('Functional Tests', () => {
-
+let Translator = require("../components/translator.js");
+let translator = new Translator();
+suite("Functional Tests", () => {
+  test("Translation with text and locale fields", (done) => {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        text: "Mangoes are my favorite fruit.",
+        locale: "american-to-british",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(
+          res.body.translation,
+          'Mangoes are my <span class="highlight">favourite</span> fruit.'
+        );
+        done();
+      });
+  });
+  test("invalid locale field", (done) => {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        text: "Mangoes are my favorite fruit.",
+        locale: "invalid",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.error, "Invalid value for locale field");
+        done();
+      });
+  });
+  test("missing text", (done) => {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        locale: "american-to-british",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.error, "Required field(s) missing");
+        done();
+      });
+  });
+  test("missing locale", (done) => {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        text: "hola",
+        locale: "",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.error, "Required field(s) missing");
+        done();
+      });
+  });
+  test("empty text", (done) => {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        text: "",
+        locale: "american-to-british",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.error, "No text to translate");
+        done();
+      });
+  });
+  test("empty text", (done) => {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        text: "hi",
+        locale: "american-to-british",
+      })
+      .end((err, res) => {
+        console.log(res.body);
+        assert.equal(res.status, 200);
+        assert.equal(res.body.translation, "Everything looks good to me!");
+        done();
+      });
+  });
 });
